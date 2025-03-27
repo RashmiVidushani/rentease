7350c4dde4c12c1f8ff3e0901b497cc2fc4c7380
Pratik B <pratikbyathnal@gmail.com>
2025-03-27 13:24:03 -0400

    Packages and file structure

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Login from "./src/components/Login/Login";
+import RenterHome from "./src/components/RenterHome/RenterHome";
+import OwnerHome from "./src/components/OwnerHome/OwnerHome";
+import Register from "./src/components/Register/Register";
+import Listing from "./src/components/Listing/Listing";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        // initialRouteName="OwnerHome"
+        initialRouteName="Login"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Login" options={{ headerShown: false }}>
+          {(props) => (
+            <Login
+              {...props}
+              title="Welcome to RentItRight"
+              buttonLabel="Login"
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name="RenterHome" component={RenterHome} />
+        <Stack.Screen name="OwnerHome" component={OwnerHome} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Listing" component={Listing} options={{headerShown: true}} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
